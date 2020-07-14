6c75eacc1d139197295ade98cbb8dec083aecba5
Stepan <goodhrommi@gmail.com>
2020-07-14 18:24:15 +0300

    add load feed and render post

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import * as yup from 'yup';
+import axios from 'axios';
+import uniqueId from 'lodash/uniqueId';
 import watch from './watchers';
+import parse from './parser';
 import createForm from './components/form';
+import createFeeds from './components/feeds';
 
 const urlSchema = yup.string().url().required();
 
@@ -25,6 +29,7 @@ const validateUrl = (url, feeds) => {
 export default () => {
   const rootElement = document.getElementById('point');
   rootElement.appendChild(createForm());
+  rootElement.appendChild(createFeeds());
 
   const state = {
     form: {
@@ -37,12 +42,15 @@ export default () => {
       valid: true,
     },
     feeds: [],
+    posts: [],
   };
 
   const elements = {
     form: document.querySelector('.rss-form'),
     urlInput: document.querySelector('.rss-form [name="url"]'),
     submitButton: document.querySelector('.rss-form [type="submit"]'),
+    feedback: document.querySelector('.feedback'),
+    feeds: document.querySelector('.feeds'),
   };
 
   const watchedState = watch(state, elements);
@@ -66,5 +74,29 @@ export default () => {
 
   elements.form.addEventListener('submit', (e) => {
     e.preventDefault();
+    watchedState.form.state = 'loading';
+    watchedState.form.error = null;
+    axios.get(`https://cors-anywhere.herokuapp.com/${watchedState.form.url.value}`)
+      .then((response) => {
+        const feed = parse(response.data);
+        const feedId = uniqueId();
+        watchedState.posts.push(...feed.items.map((item) => ({
+          ...item,
+          feedId,
+        })));
+        watchedState.feeds.unshift({
+          id: feedId,
+          title: feed.title,
+          description: feed.description,
+          url: watchedState.form.url.value,
+        });
+        watchedState.form.url.value = '';
+        watchedState.form.state = 'filling';
+        watchedState.form.error = null;
+      })
+      .catch((error) => {
+        watchedState.form.state = 'failed';
+        watchedState.form.error = error.message;
+      });
   });
 };
--- src/watchers.js
+++ src/watchers.js
@@ -11,10 +11,63 @@ export default (initialState, elements) => {
     }
   };
 
+  const handleForm = (state) => {
+    const { urlInput, submitButton, feedback } = elements;
+    switch (state.form.state) {
+      case 'filling': {
+        urlInput.value = '';
+        submitButton.removeAttribute('disabled');
+        feedback.classList.add('text-success');
+        feedback.textContent = 'Rss has been loaded';
+        break;
+      }
+      case 'loading': {
+        submitButton.setAttribute('disabled', true);
+        feedback.classList.remove('text-success', 'text-danger');
+        feedback.textContent = '';
+        break;
+      }
+      case 'failed': {
+        submitButton.removeAttribute('disabled');
+        feedback.classList.add('text-danger');
+        feedback.textContent = state.form.error;
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  };
+
+  const handleFeeds = (state) => {
+    const { feeds } = elements;
+    const html = state.feeds.map((feed) => {
+      const titleHtml = `<h2><a href="${feed.link}" target="_blank">${feed.title}</a></h2>`;
+      const descriptionHtml = `<p>${feed.description}</p>`;
+      const feedPosts = state.posts.filter((post) => post.feedId === feed.id);
+      const itemsHtml = feedPosts
+        .map((post) => (
+          `<div><a href="${post.link}">${post.title}</a></div>`
+        ))
+        .join('\n');
+      return `${titleHtml}\n${descriptionHtml}\n${itemsHtml}`;
+    });
+    feeds.innerHTML = html;
+  };
+
   const watchedState = onChange(initialState, (path) => {
+    console.log(path);
     switch (path) {
       case 'form.url.valid': {
         handleUrlInput(initialState);
+        break;
+      }
+      case 'form.state': {
+        handleForm(initialState);
+        break;
+      }
+      case 'feeds': {
+        handleFeeds(initialState);
         break;
       }
       default: {
